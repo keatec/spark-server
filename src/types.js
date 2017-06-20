b19142714b90733b175c9978c3601f4cb419843a
Andreas Häferer <ahr@ttDev.local>
2017-06-20 14:20:30 +0200

    added missing types for bunyan to types.js

--- src/types.js
+++ src/types.js
@@ -238,16 +238,22 @@ export interface ILoggerCreate {
   static createModuleLogger(aModule: any): ILogger;
 }
 export interface ILogger {
+  debug (): boolean;
   debug (line: string): void;
   debug (additional: ?Object, line: string): void;
+  error (): boolean;
   error (line: string): void;
   error (additional: ?Object, line: string): void;
+  fatal (): boolean;
   fatal (line: string): void;
   fatal (additional: ?Object, line: string): void;
+  info (): boolean;
   info (line: string): void;
   info (additional: ?Object, line: string): void;
+  warn (): boolean;
   warn (line: string): void;
   warn  (additional: ?Object, line: string): void;
+  trace (): boolean;
   trace (line: string): void;
   trace  (additional: ?Object, line: string): void;
 }
